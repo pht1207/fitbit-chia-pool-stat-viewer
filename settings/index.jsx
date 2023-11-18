ac0904d836cdb210e3c7537984399f49637eae87
Parker Throneberry <parkerthroneberry@gmail.com>
2023-11-17 22:33:25 -0600

    Adjusted settings page for altered settings

--- settings/index.jsx
+++ settings/index.jsx
@@ -2,47 +2,12 @@ function fitbitScreenDocumentSettings(props) {
   return (
     <Page>
       <Section
-        title={<Text bold align="center">Required Settings</Text>}>
-        <Select
-          label={'Coin Type'}
-          settingsKey="coin"
-          options={[
-            {name: 'xch'},
-            {name: 'etc'},
-            //{name: 'zil'},
-            {name: 'iron'},
-          ]}
-        />
-        
+        title={<Text bold align="center">Required Settings</Text>}>        
         <TextInput
-          label="Wallet Address"
-          settingsKey="address"
+          label="Launcher ID"
+          settingsKey="launcherID"
         />
         </Section>
- {/*}       <Section
-        title={<Text bold align="center">Data Display Settings</Text>}>
-
-        <Toggle
-          label="Current Effective Hashrate"
-          settingsKey="currentEffectiveHash"
-        />
-        <Toggle
-          label="Average Effective Hashrate"
-          settingsKey="avgEffectiveHash"
-        />
-        <Toggle
-          label="Reported Hashrate"
-          settingsKey="reportedHash"
-        />
-        <Toggle
-          label="Stale Shares Percentage"
-          settingsKey="staleSharesPercentage"
-        />
-        <Toggle
-          label="Invalid Share Percentage"
-          settingsKey="invalidSharesPercentage"
-        />
-        </Section>*/}
     </Page>
   );
 }
