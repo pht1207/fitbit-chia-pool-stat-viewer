2fd590dcd415b4de0f0f8f9496a7accf11803b58
Parker Throneberry <parkerthroneberry@gmail.com>
2023-11-17 23:40:01 -0600

    Changed variable names to be more appropriate, both JS and HTML

--- app/index.js
+++ app/index.js
@@ -3,19 +3,13 @@ import * as document from "document";
 import * as messaging from "messaging";
 
 //Creates text for the HTML elements that just has 'loading...' inside, for placeholder while content is fetched.
-const effectiveHashHTML = document.getElementById("averageEffectiveHash");
-effectiveHashHTML.text = `Attempting to query Flexpool.io...`;
+const farmerName = document.getElementById("farmerName");
+farmerName.text = `Attempting to query Flexpool.io...`;
 
-const reportedHashHTML = document.getElementById("reportedHash");
+const tib_24h = document.getElementById("tib_24h");
 
-const staleSharePercentageHTML = document.getElementById("staleSharePercentage");
+const currentEffort = document.getElementById("currentEffort");
 
-const invalidSharePercentageHTML = document.getElementById("invalidSharePercentage");
-
-
-const farmerName = "";
-const tib_24h = "";
-const currentEffort = ""
 
 
 //Sends a request to the companion app to fetch data from flexpool API.
@@ -30,36 +24,16 @@ function fetchFlexpoolData() {
 
 function processFlexpoolData(data) {
   //Changes the text of each HTML element to the appropriate value.
-  //Displays data in format suitable to XCH
-  /*if(data.coin == 'xch'){
-    if(data.averageEffectiveHash > 1000000000000000){
-      effectiveHashHTML.text = `Avg Effective Space: ${(data.averageEffectiveHash/1000000000000000).toFixed(2)}PB`;
-    }
-    else{
-      effectiveHashHTML.text = `Avg Effective Space: ${(data.averageEffectiveHash/1000000000000).toFixed(2)}TB`;
-    }
-    if(data.reportedHash > 1000000000000000){
-      reportedHashHTML.text = `Reported Space: ${(data.reportedHash/1000000000000).toFixed(2)}PB`;
-    }
-    else{
-      reportedHashHTML.text = `Reported Space: ${(data.reportedHash/1000000000000).toFixed(2)}TB`;
-    }
-    staleSharePercentageHTML.text = `Stale Share Percentage: ${((data.staleShares/data.validShares)*100).toFixed(2)}%`;
-    invalidSharePercentageHTML.text = `Invalid Share Percentage: ${((data.invalidShares/data.validShares)*100).toFixed(2)}%`;
-  }*/
-
   if(data.type == 'farmer'){
-    effectiveHashHTML.text = 'Farmer name: '+data.farmer_name;
-    reportedHashHTML.text = 'Avg TiB/24hr: '+data.tib_24h;
-    staleSharePercentageHTML.text = 'Current Effort: '+data.current_effort+'%';
+    farmerName.text = 'Farmer name: '+data.farmer_name;
+    tib_24h.text = 'Avg TiB/24hr: '+data.tib_24h;
+    currentEffort.text = 'Current Effort: '+data.current_effort+'%';
   }
-
-
 
   //Final case if there is an error
   else if(data.error){
     if(data.error == 'FarmerNotFound'){
-      effectiveHashHTML.text = `Farmer not found`;
+      farmerName.text = `Farmer not found`;
     }
   }
 
